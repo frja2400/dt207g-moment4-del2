0e334a5341f19b53a4d347bf8cb97172b15187c9
Frida Jansson <frja2400@student.miun.se>
2025-05-11 20:05:14 +0200

    Flyttar tillbaka html-sidorna och försöker att definiera html-filerna i main.js med defineConfig.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,20 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        login: path.resolve(__dirname, 'login.html'),
+        admin: path.resolve(__dirname, 'admin.html'),
+        register: path.resolve(__dirname, 'register.html')
+      }
+    }
+  }
+})
+
+
 const menu = document.getElementById("menu");
 const userList = document.getElementById("userList");
 
